7b5b26dd91602c58584a52fb660f57b2d54c2ca1
paulinhabruno <mapalurj@gmail.com>
2022-01-04 20:34:24 -0300

    novos exercícios

---
+++ AulaD2/exerc-d2a.js
@@ -0,0 +1,9 @@
+var vel = 50.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){ //condição simples
+    //esse comando só acontecerá se a condição acima for true
+    console.log(`Você está dirigindo na velocidade de ${vel}Km/h. Está acima do limite DE 60Km/h. MULTADO!`)
+}
+console.log(`Dirija sempre com segurança!`)
