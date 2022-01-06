962d61a5ebfe6115269e9930aeaeffd94a5d1721
paulinhabruno <mapalurj@gmail.com>
2022-01-06 08:57:32 -0300

    exercício contando passo a passo concluído

--- AulaF/ExF1/scriptExF1.js
+++ AulaF/ExF1/scriptExF1.js
@@ -7,24 +7,33 @@ function contar (){
     let res = document.querySelector('#res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || pulo.value.length == 0){
-        alert('Erro. Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+       // alert('Erro. Faltam dados!')
     }else{
-        res.innerHTML = 'Contanto: '
-        let i = Number(inicio.value)
-        let f = Number(fim.value)
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value) // convertendo para number o valor presente no início.
         let p = Number(pulo.value)
+        let f = Number(fim.value)
 
-        // Enquanto o contador começa no INICIO, e enquanto ele for MENOR OU IGUAL ao FIM, o contator vai receber ele mesmo, mais o PASSO
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c}`
-
-            // parei no minuto 10:16 da aula
-
+        if (p <= 0){
+            alert('Passo inválido! Considerando passo 1')
+            p = 1
         }
-        
+
+        if(i < f){
+// Enquanto o contador começa no INICIO, e enquanto ele for MENOR OU IGUAL ao FIM, o contator vai receber ele mesmo, mais o PASSO
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}` // código da mãozinha no site unicode emoji. Usa o comando \u{INSERE O CÓDIGO AQUI}
+             }
+           
+        } else {
+            for (let c = i ; c >= f ; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 
 }
-
 
 
